9fe2c71d15f78d5c98f610ca70c968ea2fb6a065
Daniel Dochev <Daniel.Dochev@ada.ac.uk>
2020-01-15 12:18:33 +0000

    all of video 1 done and working

--- models/person.js
+++ models/person.js
@@ -1,1 +1,18 @@
-// Your Person class goes here!
+class Person {
+    constructor(firstName, sirName, dob){
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.toLowerCase().slice(1);
+        this.sirName = sirName;
+        this.dob = dob;
+        this.emails = []
+    }
+
+    fullName() {
+        return this.firstName + ' ' + this.sirName
+    }
+
+    addEmail(email){
+        this.emails.push(email);
+    }
+}
+
+module.exports = Person;
--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,33 @@
 const chai = require('chai')
 const expect = chai.expect
+const Person = require('../models/person.js')
+
+describe('Person', ()=>{
+    it('should initialise propery', ()=>{
+        var person = new Person('Joe', 'Bloggs', '1 Jan 1998')
+        expect(person.firstName).to.equal('Joe')
+        expect(person.sirName).to.equal('Bloggs')
+        expect(person.dob).to.equal('1 Jan 1998')
+        expect(person.emails).to.deep.equal([])
+    })
+
+    it('should format first name to stat with capital and the lower case', ()=>{
+        var person = new Person('beN', 'Bloggs', '1 Jan 1998')
+        expect(person.firstName).to.equal('Ben')
+    })
+
+    it('should return full name', ()=>{
+        var person = new Person('Gregory', 'Bagins', '1 Jan 1998')
+        expect(person.fullName()).to.equal('Gregory Bagins')
+    })
+
+    it('should return emails', ()=>{
+        var person = new Person('Jon', 'Bagins', '1 Jan 1998')
+        person.addEmail('jon@ig.com')
+        person.addEmail('jon2@ig.com')
+
+        expect(person.emails).to.deep.equal(['jon@ig.com', 'jon2@ig.com'])
+    })
+})
 
 // Your Person test goes here!
